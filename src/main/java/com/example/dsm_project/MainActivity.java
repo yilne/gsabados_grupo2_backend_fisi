5a1bf02fdff9974fdbb026699cf9a7a8917146a2
Gilner Papa <85714360+yilne@users.noreply.github.com>
2022-01-27 12:43:49 -0500

    T - 12 DESARROLLAR BACK END REFUGIO

--- src/main/java/com/example/dsm_project/MainActivity.java
+++ src/main/java/com/example/dsm_project/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.dsm_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton verMas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verMas = findViewById(R.id.imageCheck);
+
+        verMas.setOnClickListener(view -> startActivity(new Intent(MainActivity.this,modal_solicitud.class)));
     }
+
+
 }
